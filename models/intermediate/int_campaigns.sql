569de407a5a1609c1cb62c46b5f3bcc8b9fc85ce
data.analytics.ressources+wagon@gmail.com <data.analytics.ressources+wagon@gmail.com>
2026-03-31 18:04:16 +0000

    staging and intermediate model for the campaigns

---
+++ models/intermediate/int_campaigns.sql
@@ -0,0 +1,11 @@
+ SELECT *
+ FROM {{ref('stg_raw__raw_adwords')}}
+ UNION ALL
+ SELECT *
+ FROM {{ref('stg_raw__raw_bing')}}
+ UNION ALL
+ SELECT *
+ FROM {{ref('stg_raw__raw_criteo')}}
+ UNION ALL
+ SELECT *
+ FROM {{ref('stg_raw__raw_facebook')}}
